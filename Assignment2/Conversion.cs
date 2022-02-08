fa53d91cec268704ff1acccd93084e09e506e750
Jimi Parekh <jimikanoja@yahoo.com>
2022-02-07 19:41:17 -0500

    Completed Conversion Class

---
+++ Assignment2/Conversion.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class Conversion
+    {
+        //declare private integer variable
+        private int userAction;
+        private double result;
+
+        //Default constructor
+        public Conversion()
+        {
+            this.userAction = 1;
+        }
+
+        //parameterized constcuctor
+        public Conversion(int userAction)
+        {
+            this.userAction = userAction;
+        }
+              
+        //ConvertCelciusToFahrenheit  
+        public double ConvertCelciusToFahrenheit()
+        {
+            // convertion of C to F = °C  x  9/5 + 32
+            result = ((userAction * 9) / 5) + 32;
+            return result;
+        }
+
+        //ConvertCelciusToKelvin  
+        public double ConvertCelciusToKelvin()
+        {
+            // convertion of C to K = ℃ + 273.15
+            result = (userAction + 273.15);
+            return result;
+        }
+
+        //ConvertKelvinToFahrenheit  
+        public double ConvertKelvinToFahrenheit()
+        {
+            // convertion of K to F = 1.8 x (K - 273) + 32 
+            result = (1.8 * (userAction - 273) + 32);
+            return result;
+        }
+
+        //ConvertKelvinToCelcius
+        public double ConvertKelvinToCelcius()
+        {
+            // convertion of K to C = K - 273
+            result = (userAction - 273);
+            return result;
+        }
+
+        //ConvertFahrenheitToKelvin
+        public double ConvertFahrenheitToKelvin()
+        {
+            // convertion of F to K = (y °F – 32) ÷ 1.8 + 273.15
+            result = ((userAction - 32) / 1.8) + 273.15;
+            return result;
+        }
+
+        //ConvertFahrenheitToCelcius
+        public double ConvertFahrenheitToCelcius()
+        {
+            // convertion of F to C
+            result = ((userAction - 32) / 9) * 5;
+            return result;
+        }
+    }
+}
--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -6,11 +6,84 @@ using System.Threading.Tasks;
 
 namespace Assignment2
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-
+            Program prg = new Program();
+            prg.Go();
+            Console.ReadKey();
         }
+
+        public void Go()
+        {
+
+            string cValue;
+            int convertValue;
+            do
+            {
+                Console.WriteLine("Enter the value to be converted: ");
+                cValue = Console.ReadLine();
+            } while (!int.TryParse(cValue, out convertValue) || (convertValue < 1));
+
+            Console.WriteLine("*****Temperature Conversion*****");
+            Console.WriteLine("What temperature do you wish to convert?");
+            Console.WriteLine("Press 1 for Celcius to Fahrenheit");
+            Console.WriteLine("Press 2 for Celcius to Kelvin");
+            Console.WriteLine("Press 3 for Kelvin to Fahrenheit");
+            Console.WriteLine("Press 4 for Kelvin to Celcius");
+            Console.WriteLine("Press 5 for Fahrenheit to Kelvin");
+            Console.WriteLine("Press 6 for Fahrenheit to Celcius");
+            Console.WriteLine("Press 7 for Exit");
+
+            int userAction;
+            string uChoice = string.Empty;
+            do
+            {
+                Console.WriteLine("Select any options from 1 to 7::");
+                uChoice = Console.ReadLine();
+
+            } while (!int.TryParse(uChoice, out userAction) || (userAction < 1) || (userAction > 7));
+
+            Conversion conversion = new Conversion(convertValue);
+            double result;
+            switch (userAction)
+            {
+                case 1:
+                    result = conversion.ConvertCelciusToFahrenheit();
+                    Console.WriteLine(result);
+                    break;
+
+                case 2:
+                    result = conversion.ConvertCelciusToKelvin();
+                    Console.WriteLine(result);
+                    break;
+
+                case 3:
+                    result = conversion.ConvertFahrenheitToCelcius();
+                    Console.WriteLine(result);
+                    break;
+                case 4:
+
+                    result = conversion.ConvertFahrenheitToKelvin();
+                    Console.WriteLine(result);
+                    break;
+
+                case 5:
+                    result = conversion.ConvertKelvinToCelcius();
+                    Console.WriteLine(result);
+                    break;
+                case 6:
+                    result = conversion.ConvertKelvinToFahrenheit();
+                    Console.WriteLine(result);
+                    break;
+                case 7:
+                    return;
+                default:
+                    Console.WriteLine("Your choicee is wrong");
+                    break;
+            }//Switch case ends here
+
+        }//Go function ends here
     }
 }
